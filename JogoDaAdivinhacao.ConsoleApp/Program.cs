1f8e9786e7b884c8ecddb0481676e39fe9fa6bc5
Igor Mota <igormello399@gmail.com>
2026-03-09 16:17:19 -0300

    implementação de verificar se o usuario quer ou não continuar

--- JogoDaAdivinhacao.ConsoleApp/Program.cs
+++ JogoDaAdivinhacao.ConsoleApp/Program.cs
@@ -1,17 +1,51 @@
 ﻿using System.Security.Cryptography;
 
+
+
+while (true)
+
+{
+
+Console.Clear();
+
 System.Console.WriteLine("-----------------------------");
 System.Console.WriteLine("JOGO DA ADIVINHAÇÃO");
 System.Console.WriteLine("-----------------------------");
 
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1,21);
 
-Console.ReadLine();
-
 System.Console.Write("Digite um valor entre 1 e 20: ");
-string chute = Console.ReadLine();
+int chute = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine($"O valor digitado foi:  {chute}");
 System.Console.WriteLine($"O valor era: {numeroAleatorio}");
 
+if (chute == numeroAleatorio)
+{
+System.Console.WriteLine("-----------------------------");
+System.Console.WriteLine("PARABÉNS VOCÊ ACERTOU");
+System.Console.WriteLine("-----------------------------");
+}
+else if (chute > numeroAleatorio)
+{
+System.Console.WriteLine("-----------------------------");
+System.Console.WriteLine("O NUMERO DIGITADO FOI MAIOR QUE O NUMERO SECRETO");
+System.Console.WriteLine("-----------------------------");
+}
+else
+{
+System.Console.WriteLine("-----------------------------");
+System.Console.WriteLine("O NUMERO DIGITADO FOI MENOR QUE O NUMERO SECRETO");
+System.Console.WriteLine("-----------------------------");
+}
 
+Console.Write("Deseja continuar (s/n)");
+string opcao =  Console.ReadLine();
+
+if (opcao.ToUpper() == "n" || opcao.ToUpper() == "N")
+    {
+         return;
+    }
+    
+       
+    }
