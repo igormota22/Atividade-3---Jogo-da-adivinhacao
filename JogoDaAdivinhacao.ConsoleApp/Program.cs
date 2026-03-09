875e737758a72b807902018afe5ebb5880dc7ed1
Igor Mota <igormello399@gmail.com>
2026-03-09 15:18:06 -0300

    inicio

--- JogoDaAdivinhacao.ConsoleApp/Program.cs
+++ JogoDaAdivinhacao.ConsoleApp/Program.cs
@@ -3,3 +3,12 @@ System.Console.WriteLine("JOGO DA ADIVINHAÇÃO");
 System.Console.WriteLine("-----------------------------");
 
 
+Console.ReadLine();
+
+System.Console.Write("Digite um valor entre 1 e 20: ");
+string chute = Console.ReadLine();
+
+
+
+
+System.Console.WriteLine($"O valor digitado foi:  {chute}");
