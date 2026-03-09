afe60f3523b70fc247ed780cc24fdac69f38b095
Igor Mota <igormello399@gmail.com>
2026-03-09 15:34:19 -0300

    implementação de numero aleatorio

--- JogoDaAdivinhacao.ConsoleApp/Program.cs
+++ JogoDaAdivinhacao.ConsoleApp/Program.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using System.Security.Cryptography;
+
 System.Console.WriteLine("-----------------------------");
 System.Console.WriteLine("JOGO DA ADIVINHAÇÃO");
 System.Console.WriteLine("-----------------------------");
 
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1,21);
 
 Console.ReadLine();
 
@@ -10,5 +12,6 @@ System.Console.Write("Digite um valor entre 1 e 20: ");
 string chute = Console.ReadLine();
 
 System.Console.WriteLine($"O valor digitado foi:  {chute}");
+System.Console.WriteLine($"O valor era: {numeroAleatorio}");
 
 
